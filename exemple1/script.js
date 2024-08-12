a1b4b848eb81eea3b7516b935b8e7228468980d8
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-08-12 09:47:34 +0100

    Update script.js

--- exemple1/script.js
+++ exemple1/script.js
@@ -4,11 +4,13 @@ const blogs = [
         image: "img/13.webp",
         description: "Aenean vel turpis scelerisque, viverra nulla in, dapibus velit. Nulla facilisi. Curabitur ut pulvinar augue."
     },
+    
     {
         title: "Blog Title 2",
         image: "img/12.webp",
         description: "Sed ac fermentum massa. Mauris consectetur, quam in tristique interdum, mauris orci efficitur justo, eu finibus eros orci ac est."
     },
+    
     {
         title: "Blog Title 3",
         image: "img/11.webp",
@@ -234,30 +236,30 @@ const top_blogs = [
         description: "Aenean vel turpis scelerisque, viverra nulla in, dapibus velit. Nulla facilisi. Curabitur ut pulvinar augue."
     },]
 
+
 let currentPage = 1;
 const postsPerPage = 6;
 const totalPages = Math.ceil(blogs.length / postsPerPage);
 
 function displayBlogs() {
-const blogSection = document.getElementById('blog-section');
-blogSection.innerHTML = '';
-const start = (currentPage - 1) * postsPerPage;
-const end = start + postsPerPage;
-for (let i = start; i < end && i < blogs.length; i++) {
-    const blog = blogs[i];
-    const blogCard = document.createElement('div');
-    blogCard.className = 'blog-card';
-    blogCard.innerHTML = `
-        <img src="${blog.image}" alt="${blog.title}">
-        <div class="blog-content">
-            <h3>${blog.title}</h3>
-            <p>${blog.description}</p>
-        </div>
-    `;
-    blogSection.appendChild(blogCard);
-}
-
-updatePagination();
+    const blogSection = document.getElementById('blog-section');
+    blogSection.innerHTML = '';
+    const start = (currentPage - 1) * postsPerPage;
+    const end = start + postsPerPage;
+    for (let i = start; i < end && i < blogs.length; i++) {
+        const blog = blogs[i];
+        const blogCard = document.createElement('div');
+        blogCard.className = 'blog-card';
+        blogCard.innerHTML = `
+            <img src="${blog.image}" alt="${blog.title}">
+            <div class="blog-content">
+                <h3>${blog.title}</h3>
+                <p>${blog.description}</p>
+            </div>
+        `;
+        blogSection.appendChild(blogCard);
+    }
+    updatePagination();
 }
 
 function updatePagination() {
@@ -311,25 +313,24 @@ pagination.appendChild(nextButton);
 }
 
 function displayTopBlogs() {
-const topblogSection = document.getElementById('top-blog-section');
-topblogSection.innerHTML = '';
-for (let i = 0; i < top_blogs.length; i++) {
-    const blog = top_blogs[i];
-    const blogCard = document.createElement('div');
-    blogCard.className = 'blog-card';
-    blogCard.innerHTML = `
-        <img src="${blog.image}" alt="${blog.title}">
-        <div class="blog-content">
-            <h3>${blog.title}</h3>
-            <p>${blog.description}</p>
-        </div>
-    `;
-    topblogSection.appendChild(blogCard);
-}
+    const topblogSection = document.getElementById('top-blog-section');
+    topblogSection.innerHTML = '';
+    for (let i = 0; i < top_blogs.length; i++) {
+        const blog = top_blogs[i];
+        const blogCard = document.createElement('div');
+        blogCard.className = 'blog-card';
+        blogCard.innerHTML = `
+            <img src="${blog.image}" alt="${blog.title}">
+            <div class="blog-content">
+                <h3>${blog.title}</h3>
+                <p>${blog.description}</p>
+            </div>
+        `;
+        topblogSection.appendChild(blogCard);
+    }
 
 }
 
-// Initial display
 displayBlogs();
 displayTopBlogs();
 
