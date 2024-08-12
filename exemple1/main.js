71206309255612cb89273d29c67544a2e869de31
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-08-12 09:46:29 +0100

    Update main.js

--- exemple1/main.js
+++ exemple1/main.js
@@ -2,11 +2,8 @@
 const showMenu = (toggleId, navId) =>{
    const toggle = document.getElementById(toggleId),
          nav = document.getElementById(navId)
-
    toggle.addEventListener('click', () =>{
-       // Add show-menu class to nav menu
        nav.classList.toggle('show-menu')
-       // Add show-icon to show and hide menu icon
        toggle.classList.toggle('show-icon')
    })
 }
@@ -16,19 +13,12 @@ showMenu('nav-toggle','nav-menu')
 /*=============== SHOW DROPDOWN MENU ===============*/
 const dropdownItems = document.querySelectorAll('.dropdown__item')
 
-// 1. Select each dropdown item
 dropdownItems.forEach((item) =>{
     const dropdownButton = item.querySelector('.dropdown__button') 
 
-    // 2. Select each button click
     dropdownButton.addEventListener('click', () =>{
-        // 7. Select the current show-dropdown class
         const showDropdown = document.querySelector('.show-dropdown')
-        
-        // 5. Call the toggleItem function
         toggleItem(item)
-
-        // 8. Remove the show-dropdown class from other items
         if(showDropdown && showDropdown!== item){
             toggleItem(showDropdown)
         }
